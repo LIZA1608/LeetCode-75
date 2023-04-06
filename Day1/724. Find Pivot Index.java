e20f3014bca5488b24f60e13f8f6894e7ddcf1e1
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-04-06 22:45:27 +0530

    Create 724. Find Pivot Index.java

---
+++ Day1/724. Find Pivot Index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+int left=0;
+int right=0;
+
+        for(int i=0;i<n;i++){
+        right+=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            right-=nums[i];
+            if(right==left){
+                return i;
+            }
+            left+=nums[i];
+        }
+        return -1;
+    }
+}
